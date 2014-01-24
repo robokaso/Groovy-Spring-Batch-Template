4a6eabab2f61844db3a8f83a3dd09f04fe76bdc8
Christian LIPP <Christian.Lipp@corba.at>
2014-01-24 10:35:17 +0100

    Formatting

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,8 +1,9 @@
 appender("stdout", ConsoleAppender) {
-  encoder(PatternLayoutEncoder) {
-	pattern = "%d [%thread] [%level] %logger - %msg%n"
-  }
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d [%thread] [%level] %logger - %msg%n"
+    }
 }
+
 logger("org.springframework.batch", INFO)
 logger("example", INFO)
 root(WARN, ["stdout"])
